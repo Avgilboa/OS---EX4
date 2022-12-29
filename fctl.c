9df75b32e5368a9f7ccebeaa0dcb4d1dac032b13
shmooel28 <shmooel28@gmail.com>
2022-12-29 10:43:05 +0200

    fctl

--- fctl.c
+++ fctl.c
@@ -1,8 +1,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
+    time_t start = time(NULL);
     int fd;
     struct flock lock;
 
@@ -16,22 +18,23 @@ int main(int argc, char *argv[]) {
     lock.l_whence = SEEK_SET;
     lock.l_start = 0;
     lock.l_len = 0;
+    for(int i=0;i<1000000;i++)
+    {
+        lock.l_type = F_LOCK;
+        if (fcntl(fd, F_SETLK, &lock) == -1) {
+            perror("Error locking file");
+            return 1;
+        }
 
-    if (fcntl(fd, F_SETLK, &lock) == -1) {
-        perror("Error locking file");
-        return 1;
+        lock.l_type = F_UNLCK;
+
+        if (fcntl(fd, F_SETLK, &lock) == -1) {
+            perror("Error unlocking file");
+            return 1;
+        }
     }
-
-    getchar();
-
-    lock.l_type = F_UNLCK;
-
-    if (fcntl(fd, F_SETLK, &lock) == -1) {
-        perror("Error unlocking file");
-        return 1;
-    }
-
     close(fd);
-
+    printf("wake a task using signal fctl: ");
+    printf("%.4f second \n", (double)(time(NULL) - start));
     return 0;
 }
